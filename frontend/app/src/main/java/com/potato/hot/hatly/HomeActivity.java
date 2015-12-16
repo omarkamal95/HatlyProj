64495e9fd85a5ac9d58d36f12ba4fc7219edf667
nelsammak <nadah.125@gmail.com>
2015-12-16 09:00:57 +0200

    y

--- frontend/app/src/main/java/com/potato/hot/hatly/HomeActivity.java
+++ frontend/app/src/main/java/com/potato/hot/hatly/HomeActivity.java
@@ -108,7 +108,6 @@ public class HomeActivity extends AppCompatActivity {
         mVisible = true;
        // mControlsView = findViewById(R.id.fullscreen_content_controls);
 
-
         // Set up the user interaction to manually show or hide the system UI.
 //        mContentView.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -127,7 +126,28 @@ public class HomeActivity extends AppCompatActivity {
         // while interacting with the UI.
        // findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
 
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            PostFragment firstFragment = new PostFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
     }
 
 
